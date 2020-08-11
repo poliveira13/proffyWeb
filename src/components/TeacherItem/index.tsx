b3151fd095f596d4a27807de6c92784d12c0a7e2
Priscila Oliveira <poliveira13@hotmail.com>
2020-08-11 01:44:03 -0300

    add new connections e whatsapp

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -4,6 +4,8 @@ import whatsappIcon from "../../assets/images/icons/whatsapp.svg";
 
 import "./styles.css";
 
+import api from "../../services/api";
+
 export interface Teacher {
   id: number;
   avatar: string;
@@ -19,6 +21,12 @@ interface TeacherItemProps {
 }
 
 const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+  function createNewConnection() {
+    api.post("connections", {
+      user_id: teacher.id,
+    });
+  }
+
   return (
     <article className="teacher-item">
       <header>
@@ -36,10 +44,14 @@ const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
           <strong>R$ {teacher.cost}</strong>
         </p>
 
-        <button type="button">
+        <a
+          target="_blank"
+          onClick={createNewConnection}
+          href={`https://wa.me/${teacher.whatsapp}`}
+        >
           <img src={whatsappIcon} alt="Whatsapp" />
           Entrar em contato
-        </button>
+        </a>
       </footer>
     </article>
   );
