7ffd69c3810a4d1c0dd486b3e0df81f68f5c2adb
Priscila Oliveira <poliveira13@hotmail.com>
2020-08-11 01:35:32 -0300

    pagina de listagem de professores finalizada as requisicoes

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -4,31 +4,36 @@ import whatsappIcon from "../../assets/images/icons/whatsapp.svg";
 
 import "./styles.css";
 
-function TeacherItem() {
+export interface Teacher {
+  id: number;
+  avatar: string;
+  bio: string;
+  cost: number;
+  name: string;
+  subject: string;
+  whatsapp: string;
+}
+
+interface TeacherItemProps {
+  teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
   return (
     <article className="teacher-item">
       <header>
-        <img
-          src="https://avatars2.githubusercontent.com/u/42249540?s=460&u=a9098559adb5df5b47a823b8256d508ef6a81659&v=4"
-          alt="Priscila Oliveira"
-        />
+        <img src={teacher.avatar} alt={teacher.name} />
         <div>
-          <strong>Priscila Oliveira</strong>
-          <span>QuÍmica</span>
+          <strong>{teacher.name}</strong>
+          <span>{teacher.subject}</span>
         </div>
       </header>
-      <p>
-        Entusiasta das melhores tecnologias de química avançada.
-        <br></br>
-        Apaixonada por explodir coisas em laboratório e por mudar a vida das
-        pessoas através de experiências. Mais de 200.000 pessoas já passaram por
-        uma das minhas explosões.
-      </p>
+      <p>{teacher.bio}</p>
 
       <footer>
         <p>
           Preço/hora
-          <strong>R$ 100,00</strong>
+          <strong>R$ {teacher.cost}</strong>
         </p>
 
         <button type="button">
@@ -38,6 +43,6 @@ function TeacherItem() {
       </footer>
     </article>
   );
-}
+};
 
 export default TeacherItem;
